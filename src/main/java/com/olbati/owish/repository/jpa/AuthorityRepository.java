6d37e295c11722e2d134a5a24181c26fa5278a01
marouen <marouen.ouerghemmi@b2m-it.com>
2017-01-31 15:35:41 +0100

    security

---
+++ src/main/java/com/olbati/owish/repository/jpa/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.olbati.owish.repository.jpa;
+
+
+import com.olbati.owish.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+
+}
---
+++ src/main/java/com/olbati/owish/repository/jpa/PersistentTokenRepository.java
@@ -0,0 +1,20 @@
+package com.olbati.owish.repository.jpa;
+
+
+import com.olbati.owish.domain.PersistentToken;
+import com.olbati.owish.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the PersistentToken entity.
+ */
+public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
